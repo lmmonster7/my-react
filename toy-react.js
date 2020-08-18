7ec9f1aeae739f438feacb45180c50b61aca9593
mingoo <18718022652@163.com>
2020-08-19 00:09:23 +0800

    feat: 添加onclick setState功能

--- toy-react.js
+++ toy-react.js
@@ -1,22 +1,43 @@
-// 这三个包装类的共同特性，都是自定义dom对象，读取其root属性返回真实dom
 
+const RENDER_TO_DOM = Symbol("render to dom")
+
+// 这三个包装类的共同特性，都是自定义dom对象，读取其root属性返回真实dom
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type)
   }
   // 原生标签的属性直接当成原生属性进行添加
   setAttribute(name, value) {
-    this.root.setAttribute(name, value)
+    if (name.match(/^on([\s\S]+)$/)) {
+      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
+    } else {
+      if (name === 'className') {
+        this.root.setAttribute('class', value)
+      } else {
+        this.root.setAttribute(name, value)
+      }
+    }
   }
   // 原生标签的子标签直接使用原生appendChild添加
   appendChild(component) {
-    this.root.appendChild(component.root)
+    let range = document.createRange()
+    range.setStart(this.root, this.root.childNodes.length)
+    range.setEnd(this.root, this.root.childNodes.length)
+    component[RENDER_TO_DOM](range)
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content)
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
@@ -34,11 +55,38 @@ export class Component {
   appendChild(component) {
     this.children.push(component)
   }
-  get root() {
-    if (!this._root) {
-      this._root = this.render().root
+  [RENDER_TO_DOM](range) {
+    this._range = range
+    this.render()[RENDER_TO_DOM](range)
+  }
+  rerender() {
+    let oldRange = this._range
+    
+    let range = document.createRange()
+    range.setStart(oldRange.startContainer, oldRange.startOffset)
+    range.setEnd(oldRange.startContainer, oldRange.startOffset)
+    this[RENDER_TO_DOM](range)
+
+    oldRange.setStart(range.endContainer, range.endOffset)
+    oldRange.deleteContents()
+  }
+  setState(newState) {
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state = newState
+      this.rerender()
+      return
     }
-    return this._root
+    let merge =  (oldState, newState) => {
+      for (let p in newState) {
+        if (oldState[p] === null || typeof oldState[p] !== 'object') {
+          oldState[p] = newState[p]
+        } else {
+          merge(oldState[p], newState[p])
+        }
+      }
+    }
+    merge(this.state, newState)
+    this.rerender()
   }
 }
 
@@ -69,6 +117,9 @@ export function createElement(type, attributes, ...children) {
       if (typeof child === 'string') {
         child = new TextWrapper(child)
       }
+      if (child === null) {
+        continue
+      }
       // 处理传入的是一个数组的情况，因为 { this.children } this.children指向的是一个数组
       if ((typeof child === 'object') && (child instanceof Array)) {
         insertChildren(child)
@@ -82,7 +133,11 @@ export function createElement(type, attributes, ...children) {
   return e
 }
 
-
+// 该函数的功能是将dom元素挂载到html上
 export function render(component, parentElement) {
-  parentElement.appendChild(component.root)
+  let range = document.createRange()
+  range.setStart(parentElement, 0)
+  range.setEnd(parentElement, parentElement.childNodes.length)
+  range.deleteContents()
+  component[RENDER_TO_DOM](range)
 }
